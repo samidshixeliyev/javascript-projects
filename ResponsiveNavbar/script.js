69dc2ab0c76ed8c4ccff242ce4cc1f183474d8fb
samidshixeliyev <samid661@gmail.com>
2025-03-17 21:14:21 +0400

    Update

---
+++ ResponsiveNavbar/script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+  const sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "flex";
+}
+function hideSidebar() {
+  const sideBar = document.querySelector(".sidebar");
+  sideBar.style.display = "none";
+}
---
+++ mapty/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude,longitude];
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+      const map = L.map("map").setView(coords, 13);
+      console.log(map);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("Elçin müəllim .<br> Səmid <br> Code Academy-də.")
+        .openPopup();
+        map.on('click',function(mapEvent){
+          const {lat,lng}=mapEvent.latlng;
+          L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+              L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+              })
+            );
+        })
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
